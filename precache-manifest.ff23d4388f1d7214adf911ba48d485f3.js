0cccb3867798d3a2ed71be083c4c10d859fa7cb0
tsharon-byte <sharonova_t@inbox.ru>
2020-04-06 12:50:45 +0300

    Updates

---
+++ precache-manifest.ff23d4388f1d7214adf911ba48d485f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff4d735f662ab20970fe4efa0c5351a",
+    "url": "/client-side-form/index.html"
+  },
+  {
+    "revision": "6a766d101bfde379359d",
+    "url": "/client-side-form/static/css/main.32619c3c.chunk.css"
+  },
+  {
+    "revision": "27f88836d36e4be813c8",
+    "url": "/client-side-form/static/js/2.e0a83b27.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/client-side-form/static/js/2.e0a83b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a766d101bfde379359d",
+    "url": "/client-side-form/static/js/main.b2acf4c3.chunk.js"
+  },
+  {
+    "revision": "919c570d0b7ae3b93590",
+    "url": "/client-side-form/static/js/runtime-main.ad1ad268.js"
+  }
+]);
