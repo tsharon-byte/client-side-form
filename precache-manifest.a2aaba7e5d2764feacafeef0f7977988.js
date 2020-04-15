3a7a289223705aae1a4075c7af4df895a11a107f
tsharon-byte <sharonova_t@inbox.ru>
2020-04-15 23:38:14 +0300

    Updates

--- precache-manifest.a2aaba7e5d2764feacafeef0f7977988.js
+++ precache-manifest.a2aaba7e5d2764feacafeef0f7977988.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e3068f97f05ef6bfd18c6cce999267c0",
+    "revision": "b7cfc51dc2f8f275c837475eebf58762",
     "url": "/client-side-form/index.html"
   },
   {
-    "revision": "993d6832257744c71799",
+    "revision": "9787faf8984b5f48dd7b",
     "url": "/client-side-form/static/css/main.a5e9eddc.chunk.css"
   },
   {
-    "revision": "e3a73407237962e69998",
-    "url": "/client-side-form/static/js/2.2da56b02.chunk.js"
+    "revision": "7990441ee1ef73830159",
+    "url": "/client-side-form/static/js/2.8e296750.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/client-side-form/static/js/2.2da56b02.chunk.js.LICENSE.txt"
+    "url": "/client-side-form/static/js/2.8e296750.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "993d6832257744c71799",
-    "url": "/client-side-form/static/js/main.35163e95.chunk.js"
+    "revision": "9787faf8984b5f48dd7b",
+    "url": "/client-side-form/static/js/main.881c8ebb.chunk.js"
   },
   {
     "revision": "919c570d0b7ae3b93590",
